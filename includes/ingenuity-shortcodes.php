c9b3ae6f850a00e1544a4bf7e54d9f7f66ebc36b
Arif <arif@anzum.com>
2015-12-29 08:09:38 +0530

    Shortcode edits

--- includes/ingenuity-shortcodes.php
+++ includes/ingenuity-shortcodes.php
@@ -10,10 +10,10 @@ if( ! function_exists('inortho_cto')) {
                         'btncolor' => '#468DB9',
                         'btntxt' => 'Get your Free Smile Analysis Today!',
                         'btnlink' => '#',
-      					'ctotitle' => '',
+      					'ctotitle' => 'Get your Free Smile Analysis Today!',
                         'ctocontent' => 'We do not want anything to get in the way of a perfect smile. We offer each and every patient we see a free smile assessment.',
                         'txtcolor' =>'#fff'
-					), $atts, 'ctotitle','bgcolor','src','btncolor','btntitle','btnlink','ctocontent','txtcolor'
+					), $atts, 'ctotitle','bgcolor','imgsrc','btncolor','btntxt','btnlink','ctocontent','txtcolor'
                  	);
     
     $cto_data = '<div style="background-color:'.$attributes["bgcolor"].'" class="inortho-cto">
@@ -25,7 +25,7 @@ if( ! function_exists('inortho_cto')) {
 							<div class="cto-content">
 								<h5 style="color:'.$attributes["txtcolor"].'">'.$attributes["ctotitle"].'</h5>
 								<p style="color:'.$attributes["txtcolor"].'" class="cto-title">'.$attributes["ctocontent"].'</p>
-								<p><a class="cto-btn" style="color:'.$attributes["txtcolor"].';background-color:'.$attributes["btncolor"].'" href="'.$attributes["btnlink"].'">'.$attributes["btntitle"].'</a></p></div>
+								<p><a class="cto-btn" style="color:'.$attributes["txtcolor"].';background-color:'.$attributes["btncolor"].'" href="'.$attributes["btnlink"].'">'.$attributes["btntxt"].'</a></p></div>
 							</div>
 						</div>
 				</div>';
